0a3d47344a16147f64c0ad4275c7f5c82a8f5e50
richard gowers <richardjgowers@gmail.com>
2023-05-31 15:38:06 +0100

    move filters into separate directory

---
+++ src/kartograf/filters/__init__.py
@@ -0,0 +1,1 @@
+from .element_change import filter_atoms_h_only_h_mapped
---
+++ src/kartograf/filters/element_change.py
@@ -0,0 +1,27 @@
+import logging
+from rdkit import Chem
+
+logger = logging.getLogger(__name__)
+
+
+def filter_atoms_h_only_h_mapped(
+    molA: Chem.Mol, molB: Chem.Mol, mapping: dict[int, int]
+) -> dict[int, int]:
+    """Forces a mapping to only allow hydrogens to map to other hydrogens"""
+
+    filtered_mapping = {}
+    for atomA_idx, atomB_idx in mapping.items():
+        atomA = molA.GetAtomWithIdx(atomA_idx)
+        atomB = molB.GetAtomWithIdx(atomB_idx)
+
+        if (atomA.GetAtomicNum() == atomB.GetAtomicNum() == 1) or (
+            atomA.GetAtomicNum() == atomB.GetAtomicNum() != 1
+        ):
+            filtered_mapping[atomA_idx] = atomB_idx
+            logger.debug(f"keep mapping for atomIDs ({atomA_idx}, {atomB_idx}):"
+                         f" {atomA.GetAtomicNum()} {atomB.GetAtomicNum()}")
+        else:
+            logger.debug(f"no mapping for atomIDs ({atomA_idx}, {atomB_idx}):"
+                         f" {atomA.GetAtomicNum()} {atomB.GetAtomicNum()}")
+
+    return filtered_mapping
