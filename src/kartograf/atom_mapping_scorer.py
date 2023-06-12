527ef14692ba3900a61986a6f41f1d5eea3f2c8b
riesben <benjamin-ries@outlook.com>
2023-06-12 13:35:24 +0200

    moving code around

---
+++ src/kartograf/atom_mapping_scorer.py
@@ -0,0 +1,60 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/kartograf
+
+import logging
+import numpy as np
+
+from gufe.mapping import AtomMapping
+
+from mapping_metrics import MappingRMSDScorer
+from mapping_metrics import MappingShapeOverlapScorer, MappingShapeMismatchScorer
+from mapping_metrics import MappingVolumeRatioScorer
+
+from mapping_metrics._abstract_scorer import _AbstractAtomMappingScorer
+
+log = logging.getLogger(__name__)
+
+
+class DefaultKartografScorer(_AbstractAtomMappingScorer):
+    """
+    Warning this is highly experimental!
+    """
+    def __init__(self):
+        """
+        this could be a future scorer using the here derined metrics?
+
+        don't use ;)
+        """
+        self.scorers = [MappingVolumeRatioScorer(),
+                        MappingShapeOverlapScorer(),
+                        MappingShapeMismatchScorer(),
+                        MappingRMSDScorer()]
+
+        self.weights = np.array([1,3,3,3])
+        self.weigths = self.weights/sum(self.weights)
+
+    def get_score(self, mapping: AtomMapping) -> float:
+        """
+            Under Development
+
+        Parameters
+        ----------
+        mapping : AtomMapping
+            AtomMapping to be scored
+
+        Returns
+        -------
+        float
+            normalized score
+        """
+        log.info("Kartograf Score:")
+        scores = []
+        for weight, scorer in zip(self.weights, self.scorers):
+            s = scorer.get_score(mapping)
+            log.info("\t"+scorer.__class__.__name__+"\t"+str(s)+"\tweight: "+str(weight))
+            scores.append(s*weight)
+
+        score = np.round(np.mean(scores),2)
+        score = score if(score<1.0) else 1.0
+        log.info("Result: "+str(score))
+        return score
---
+++ src/kartograf/mapping_metrics/_abstract_scorer.py
@@ -0,0 +1,44 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/kartograf
+
+import abc
+import logging
+import numpy as np
+
+from gufe.mapping import AtomMapping
+
+log = logging.getLogger(__name__)
+
+
+eukli = lambda x, y: np.sqrt(np.sum(np.square(y - x)))
+rms_func = lambda x: np.sqrt(np.mean(np.square(x)))
+
+
+class _AbstractAtomMappingScorer(abc.ABC):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, mapping:AtomMapping, *args, **kwargs)->float:
+        return self.get_score(mapping)
+
+    @abc.abstractmethod
+    def get_score(self, mapping:AtomMapping, *args, **kwargs)->float:
+        """
+            the scoring function returns a value between 0 and 1.
+            a value close to Zero indicates a small distance, a score close to one indicates a large cost/error.
+
+        Parameters
+        ----------
+        mapping: AtomMapping
+            the mapping to be scored
+        args
+        kwargs
+
+        Returns
+        -------
+        float
+            a value between [0,1] where one is a very bad score and 0 a very good one.
+
+        """
+        pass
