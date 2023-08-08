e7a6c89a17edf0588a172d2fa0dc9908e8eaf493
bries <benjamin-ries@outlook.com>
2023-08-08 11:29:32 +0200

    optional imports examples

--- src/kartograf/tests/test_utils_vis.py
+++ src/kartograf/tests/test_utils_vis.py
@@ -3,22 +3,27 @@ import pytest
 from py3Dmol import view
 from ipywidgets.widgets.widget_box import VBox
 
-from kartograf.utils.mapping_visualization_widget import display_mappings_3d, display_mapping_3d
+try:
+    from kartograf.utils.mapping_visualization_widget import display_mappings_3d, display_mapping_3d
+    optional_imports = True
+except ImportError:
+    optional_imports = False
 
 from .conf import stereo_chem_mapping
 
 
-def test_score_mappings_rmsd(stereo_chem_mapping):
-    """
-    Currently a smoke test
-    """
-    v = display_mapping_3d(stereo_chem_mapping)
-    assert isinstance(v, view)
+if(optional_imports):
+    def test_score_mappings_rmsd(stereo_chem_mapping):
+        """
+        Currently a smoke test
+        """
+        v = display_mapping_3d(stereo_chem_mapping)
+        assert isinstance(v, view)
 
 
-def test_view_mapping(stereo_chem_mapping):
-    """
-    Currently a smoke test
-    """
-    view = display_mappings_3d([stereo_chem_mapping,stereo_chem_mapping])
-    assert isinstance(view, VBox)
+    def test_view_mapping(stereo_chem_mapping):
+        """
+        Currently a smoke test
+        """
+        view = display_mappings_3d([stereo_chem_mapping,stereo_chem_mapping])
+        assert isinstance(view, VBox)
