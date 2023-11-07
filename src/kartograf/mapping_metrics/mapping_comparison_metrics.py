b7c86700be287cf445650543efd1f08c3558ff75
riesben <benjamin-ries@outlook.com>
2023-11-07 14:36:09 +0100

    add jaccard socre as used in publication

---
+++ src/kartograf/mapping_metrics/mapping_comparison_metrics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from gufe import AtomMapping
+
+
+def jaccard_score(mappingA:AtomMapping, mappingB:AtomMapping) -> float:
+    """
+        The Jaccard score is a normalized score ([0,1]) , that gives insight on the selected atom pair diversity of two compared mappings.
+        Diversity is expressed here as the change in atom assignments, if the score is 1 the mappings are identical. If the score is 0, the two mappings
+        consist of entirely different atom mapping pairs.
+
+    Parameters
+    ----------
+    mappingA: AtomMapping
+    mappingB: AtomMapping
+
+    Returns
+    -------
+    float
+        the calculated Jaccard Score [0, 1]
+    """
+
+    mappingA_pairs = set(mappingA.componentA_to_componentB.items())
+    mappingB_pairs = set(mappingB.componentA_to_componentB.items())
+
+    intersection = mappingA_pairs.intersection(mappingB_pairs)
+    union = mappingA_pairs.union(mappingB_pairs)
+
+    return np.round(len(intersection) / len(union), 2)
