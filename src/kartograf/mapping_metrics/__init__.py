41291fd54bb5f15a649989637a364541d99a5979
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2026-03-12 12:46:30 -0700

    docs: Update docstrings to NumPy style in __init__.py

--- src/kartograf/mapping_metrics/__init__.py
+++ src/kartograf/mapping_metrics/__init__.py
@@ -2,11 +2,16 @@
 # For details, see https://github.com/OpenFreeEnergy/kartograf
 
 from .metric_mapping_rmsd import MappingRMSDScorer as MappingRMSDScorer
+"""Compute the RMSD of a mapping."""
 from .metric_shape_difference import (
     MappingShapeMismatchScorer as MappingShapeMismatchScorer,
+"""Compute the shape mismatch of a mapping."""
     MappingShapeOverlapScorer as MappingShapeOverlapScorer,
+"""Compute the shape overlap of a mapping."""
 )
 from .metric_volume_ratio import (
     MappingRatioMappedAtomsScorer as MappingRatioMappedAtomsScorer,
+"""Compute the ratio of mapped atoms in a mapping."""
     MappingVolumeRatioScorer as MappingVolumeRatioScorer,
+"""Compute the volume ratio of a mapping."""
 )
