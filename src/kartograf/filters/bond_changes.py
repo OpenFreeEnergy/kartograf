ba401641a64c369c10292b2957672f45dbef21f2
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2026-03-12 13:36:22 -0700

    docs: Update docstrings to NumPy style in bond_changes.py

--- src/kartograf/filters/bond_changes.py
+++ src/kartograf/filters/bond_changes.py
@@ -8,25 +8,26 @@ from rdkit import Chem
 logger = logging.getLogger(__name__)
 
 
-def filter_bond_breaks(mol_a: Chem.Mol, mol_b: Chem.Mol, mapping: dict[int, int]):
+def filter_bond_breaks(mol_a: Chem.Mol, mol_b: Chem.Mol, mapping: dict[int, int]) -> dict[int, int]:
     """
     Remove cases where a bond would be broken during the transformation.
-    These are detected by checking that all bonds of ligand A which are in the mapping also form a bond in ligand B,
-    if they do not the atoms are removed from the mapping.
+    These are detected by checking that all bonds of ligand A which are in the mapping also form a bond in ligand B.
+    If they do not, the atoms are removed from the mapping.
 
     See <https://github.com/OpenFreeEnergy/kartograf/issues/88> for more details.
 
     Parameters
     ----------
-    mol_a
-        Molecule at state A with atom indices in the keys of the mapping dict
-    mol_b
-        Molecule at state B with atom indices in the values of the mapping dict
-    mapping
-        The mapping between molecule A and B
+    mol_a : Chem.Mol
+        Molecule at state A with atom indices in the keys of the mapping dict.
+    mol_b : Chem.Mol
+        Molecule at state B with atom indices in the values of the mapping dict.
+    mapping : dict[int, int]
+        The mapping between molecule A and B.
 
     Returns
     -------
+    dict[int, int]
         A filtered mapping removing any entries which map a broken bond.
     """
 
